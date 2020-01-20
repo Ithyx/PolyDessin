c27bce2b1fe37625d77c8632ee4cc243806e7195
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-01-20 15:18:43 -0500

    début de la page d'accueil

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, AccueilComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
