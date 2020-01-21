b6b6252613d34778d8954ffda970fd49f0a627fc
Ithyx <lamidey.m@gmail.com>
2020-01-20 19:55:43 -0500

    fix du component barre-outils

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { PageDessinComponent } from './components/page-dessin/page-dessin.component';
-import {RouterModule} from '@angular/router';
-import { PageGuideComponent } from './components/page-guide/page-guide.component'
+import { RouterModule } from '@angular/router';
+import { PageGuideComponent } from './components/page-guide/page-guide.component';
+import { BarreOutilsComponent } from "./components/barre-outils/barre-outils.component";
 
 
 
 @NgModule({
-    declarations: [AppComponent, AccueilComponent, PageDessinComponent, PageGuideComponent],
+    declarations: [AppComponent, AccueilComponent, PageDessinComponent, PageGuideComponent, BarreOutilsComponent],
     imports: [BrowserModule, HttpClientModule, RouterModule.forRoot([
         {path: '', component: AccueilComponent},
         {path: 'dessin', component: PageDessinComponent},
