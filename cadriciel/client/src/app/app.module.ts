f57deedaa0978d391ce6f99024863a2907739d69
Symon <symon.nestoruk@polymtl.ca>
2020-01-27 16:00:31 -0500

    arranger les import de module.ts

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from "@angular/material";
-import { AppComponent } from './components/app/app.component';
-import { AccueilComponent } from './components/accueil/accueil.component';
-import { PageDessinComponent } from './components/page-dessin/page-dessin.component';
 import { RouterModule } from '@angular/router';
-import { PageGuideComponent } from './components/page-guide/page-guide.component';
-import { GuideSujetComponent } from "./components/guide-sujet/guide-sujet.component";
-import { BarreOutilsComponent } from "./components/barre-outils/barre-outils.component";
-import { OutilDessinComponent } from './components/outil-dessin/outil-dessin.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { AppComponent } from './components/app/app.component';
+import { BarreOutilsComponent } from './components/barre-outils/barre-outils.component';
 import { FenetreNewDessinComponent } from './components/fenetre-new-dessin/fenetre-new-dessin.component';
-
+import { GuideSujetComponent } from './components/guide-sujet/guide-sujet.component';
+import { OutilDessinComponent } from './components/outil-dessin/outil-dessin.component';
+import { PageDessinComponent } from './components/page-dessin/page-dessin.component';
+import { PageGuideComponent } from './components/page-guide/page-guide.component';
 
 @NgModule({
     declarations: [AppComponent, AccueilComponent, PageDessinComponent, PageGuideComponent,
