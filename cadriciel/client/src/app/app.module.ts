3dd2a5667b3757981d9a143032dd2ae954d89561
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-01-27 14:43:16 -0500

    ajout material

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from "@angular/material";
 import { AppComponent } from './components/app/app.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { PageDessinComponent } from './components/page-dessin/page-dessin.component';
@@ -9,16 +10,18 @@ import { PageGuideComponent } from './components/page-guide/page-guide.component
 import { GuideSujetComponent } from "./components/guide-sujet/guide-sujet.component";
 import { BarreOutilsComponent } from "./components/barre-outils/barre-outils.component";
 import { OutilDessinComponent } from './components/outil-dessin/outil-dessin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 
 @NgModule({
     declarations: [AppComponent, AccueilComponent, PageDessinComponent, PageGuideComponent, BarreOutilsComponent, OutilDessinComponent, GuideSujetComponent],
-    imports: [BrowserModule, HttpClientModule, RouterModule.forRoot([
+    imports: [BrowserModule, HttpClientModule, MatButtonModule ,RouterModule.forRoot([
         {path: '', component: AccueilComponent},
         {path: 'dessin', component: PageDessinComponent},
         {path: 'guide', component : PageGuideComponent}
-    ])],
+    ]), BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
