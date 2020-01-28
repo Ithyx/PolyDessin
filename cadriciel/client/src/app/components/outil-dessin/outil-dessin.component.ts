6666be657c2bba5ad6e0b705c250a3bec2274dff
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-01-28 01:25:43 -0500

    fix linting outil-dessin

--- cadriciel/client/src/app/components/outil-dessin/outil-dessin.component.ts
+++ cadriciel/client/src/app/components/outil-dessin/outil-dessin.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-export class ParametreOutil {
-  public type: string;
-  public nom: string;
-  public options?: string[];
-  public valeur?: number = 0;
+export interface ParametreOutil {
+  type: string;
+  nom: string;
+  options?: string[];
+  valeur ?: number;
 }
 
-export class OutilDessin {
-  public nom: string;
-  public estActif: boolean;
-  public idOutil: number;
-  public parametres: ParametreOutil[];
+export interface OutilDessin {
+  nom: string;
+  estActif: boolean;
+  idOutil: number;
+  parametres: ParametreOutil[];
 }
 
 @Component({
@@ -19,15 +19,10 @@ export class OutilDessin {
   templateUrl: './outil-dessin.component.html',
   styleUrls: ['./outil-dessin.component.scss']
 })
-export class OutilDessinComponent implements OnInit {
+export class OutilDessinComponent {
 
-  @Input() public outil: OutilDessin;
-  @Output() public notification = new EventEmitter<OutilDessin>();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() outil: OutilDessin;
+  @Output() notification = new EventEmitter<OutilDessin>();
 
   onClick() {
     // Devient l'outil actif.
@@ -43,7 +38,7 @@ export const outils: OutilDessin[] = [
     estActif: true,
     idOutil: 0,
     parametres: [
-      {type: "number", nom: "Épaisseur"}
+      {type: 'number', nom: 'Épaisseur'}
     ]
   },
   {
@@ -51,8 +46,8 @@ export const outils: OutilDessin[] = [
     estActif: false,
     idOutil: 1,
     parametres: [
-      {type: "number", nom: "Épaisseur"},
-      {type: "select", nom: "Texture", options: ["Texture1", "Texture2", "Texture3", "Texture4", "Texture5"]}
+      {type: 'number', nom: 'Épaisseur'},
+      {type: 'select', nom: 'Texture', options: ['Texture1', 'Texture2', 'Texture3', 'Texture4', 'Texture5']}
     ]
   },
   {
@@ -60,8 +55,8 @@ export const outils: OutilDessin[] = [
     estActif: false,
     idOutil: 2,
     parametres: [
-      {type: "number", nom: "Épaisseur du contour"},
-      {type: "select", nom: "Type de tracé", options: ["Contour", "Plein", "Plein avec contour"]}
+      {type: 'number', nom: 'Épaisseur du contour'},
+      {type: 'select', nom: 'Type de tracé', options: ['Contour', 'Plein', 'Plein avec contour']}
     ]
   },
   {
@@ -69,9 +64,9 @@ export const outils: OutilDessin[] = [
     estActif: false,
     idOutil: 3,
     parametres: [
-      {type: "number", nom: "Épaisseur"},
-      {type: "select", nom: "Type de jonction", options: ["Avec points", "Sans points"]},
-      {type: "number", nom: "Diamètre des jonctions"}
+      {type: 'number', nom: 'Épaisseur'},
+      {type: 'select', nom: 'Type de jonction', options: ['Avec points', 'Sans points']},
+      {type: 'number', nom: 'Diamètre des jonctions'}
     ]
   },
   {
