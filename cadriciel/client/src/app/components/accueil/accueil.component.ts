ff58fcaf46c78581c46ef643861584ff4cbfccba
Symon <symnestoruk@hotmail.com>
2020-01-27 20:12:26 -0500

    link de créer dessin au pop up

--- cadriciel/client/src/app/components/accueil/accueil.component.ts
+++ cadriciel/client/src/app/components/accueil/accueil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogConfig } from '@angular/material';
+import { FenetreNewDessinComponent } from '../fenetre-new-dessin/fenetre-new-dessin.component';
 
 
 @Component({
@@ -8,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog
+    ) {}
 
+  onCreate(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    this.dialog.open(FenetreNewDessinComponent, dialogConfig)
+  }
   
 
   ngOnInit() {
