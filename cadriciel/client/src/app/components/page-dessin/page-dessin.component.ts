d0bd52b9f8eec70f2aaefcd2de284839ea838655
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-01-30 11:10:38 -0500

    refactoring des outils

--- cadriciel/client/src/app/components/page-dessin/page-dessin.component.ts
+++ cadriciel/client/src/app/components/page-dessin/page-dessin.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { DessinCrayonService } from 'src/app/services/dessin-crayon.service';
+import { GestionnaireOutilsService } from 'src/app/services/outils/gestionnaire-outils.service';
 import { StockageSvgService } from 'src/app/services/stockage-svg.service';
-import { OutilDessin } from '../outil-dessin/outil-dessin.component';
 
 @Component({
   selector: 'app-page-dessin',
@@ -10,48 +10,42 @@ import { OutilDessin } from '../outil-dessin/outil-dessin.component';
 })
 export class PageDessinComponent {
 
-  outilActif: OutilDessin = {
-    nom: 'Crayon',
-    estActif: true,
-    idOutil: 0,
-    parametres: [
-      {type: 'number', nom: 'Épaisseur'}
-    ]
-  };
+  constructor(public stockage: StockageSvgService,
+              private outils: GestionnaireOutilsService,
+              private crayon: DessinCrayonService,
+    ) { }
 
-  constructor(public stockage: StockageSvgService, private crayon: DessinCrayonService) { }
-
-  onNotify(outil: OutilDessin) {
-    console.log('nouvel outil sélectionné: ', outil)
-    this.outilActif = outil;
+  @HostListener('document:keydown', ['$event'])
+  selectCrayon(event: KeyboardEvent) {
+    console.log('crayon sélectionné touche: ', event.key);
   }
 
   onClick(mouse: MouseEvent) {
-    if (this.outilActif.nom === 'Crayon') {
+    if (this.outils.outilActif.nom === 'Crayon') {
       this.crayon.onClickCrayon(mouse);
     }
   }
 
   onMouseMove(mouse: MouseEvent) {
-    if (this.outilActif.nom === 'Crayon' && mouse.buttons === 1) {
+    if (this.outils.outilActif.nom === 'Crayon' && mouse.buttons === 1) {
       this.crayon.onMouseMoveCrayon(mouse);
     }
   }
 
   onMousePress(mouse: MouseEvent) {
-    if (this.outilActif.nom === 'Crayon') {
+    if (this.outils.outilActif.nom === 'Crayon') {
       this.crayon.onMousePressCrayon(mouse);
     }
   }
 
   onMouseRelease(mouse: MouseEvent) {
-    if (this.outilActif.nom === 'Crayon') {
+    if (this.outils.outilActif.nom === 'Crayon') {
       this.crayon.onMouseReleaseCrayon(mouse);
     }
   }
 
   onMouseLeave(mouse: MouseEvent) {
-    if (this.outilActif.nom === 'Crayon') {
+    if (this.outils.outilActif.nom === 'Crayon') {
       this.crayon.onMouseLeaveCrayon(mouse);
     }
   }
