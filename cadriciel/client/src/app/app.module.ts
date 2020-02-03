c75142bdcd4985f5fba7b6951cb1651abfc5c9c6
Timothy <timothy.chheang@polymtl.ca>
2020-02-03 15:30:22 -0500

    erreur de tim

--- cadriciel/client/src/app/app.module.ts
+++ cadriciel/client/src/app/app.module.ts
@@ -33,7 +33,7 @@ import { StockageSvgService } from './services/stockage-svg.service';
             ChoixCouleurComponent, GlissiereCouleurComponent, CouleurPaletteComponent],
     imports: [BrowserModule, HttpClientModule, MatButtonModule, FormsModule, ReactiveFormsModule,
         MatButtonModule, MatDialogModule, BrowserAnimationsModule, RouterModule.forRoot([
-        {path: '', component: ChoixCouleurComponent},
+        {path: '', component: AccueilComponent},
         {path: 'dessin', component: PageDessinComponent},
         {path: 'guide', component : PageGuideComponent}
     ])],
