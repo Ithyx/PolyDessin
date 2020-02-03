287628792d124874845f82451c7cce5606e940d8
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-02-03 13:21:14 -0500

    fix tests de new-dessin-component

--- cadriciel/client/src/app/components/fenetre-new-dessin/fenetre-new-dessin.component.spec.ts
+++ cadriciel/client/src/app/components/fenetre-new-dessin/fenetre-new-dessin.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { RouterModule } from '@angular/router';
 
 import { FenetreNewDessinComponent } from './fenetre-new-dessin.component';
 
@@ -8,7 +11,9 @@ describe('FenetreNewDessinComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FenetreNewDessinComponent ]
+      imports: [ ReactiveFormsModule, MatDialogModule, RouterModule.forRoot([]) ],
+      declarations: [ FenetreNewDessinComponent ],
+      providers: [ {provide: MatDialogRef, useValue: {}} ]
     })
     .compileComponents();
   }));
