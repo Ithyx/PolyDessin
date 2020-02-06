cd4dc2c17dbe32c7311e6bdc249c8d4b224fd5ab
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@polymtl.ca>
2020-02-05 19:04:15 -0500

    5 textures differentes et fonctionnels

--- cadriciel/client/src/app/services/outils/dessin-pinceau.service.ts
+++ cadriciel/client/src/app/services/outils/dessin-pinceau.service.ts
@@ -10,43 +10,58 @@ export class DessinPinceauService implements InterfaceOutils {
 
   constructor(public stockageSVG: StockageSvgService, public outils: GestionnaireOutilsService) { }
 
-  sourisCliquee(souris: MouseEvent) {
-    console.log('POINT PINCEAU');
-    if (this.outils.outilActif.parametres[0].valeur) {
-      const SVG = '<circle filter="url(#' + this.outils.outilActif.parametres[1].optionChoisie
-      + ')"  cx="' + souris.offsetX + '" cy="' + souris.offsetY + '" r="'
-      + this.outils.outilActif.parametres[0].valeur / 2 + '" fill="black"/>';
-      this.stockageSVG.ajouterSVG(SVG);
-    }
-    this.stockageSVG.setSVGEnCours('');
-  }
+  traitEnCours = false;
+  click = true;
 
   sourisDeplacee(souris: MouseEvent) {
-    let crayon: string = this.stockageSVG.getSVGEnCours();
+    if (this.traitEnCours) {
+      let crayon: string = this.stockageSVG.getSVGEnCours();
 
-    crayon += 'L' + souris.offsetX + ' ' + souris.offsetY + ' "/>';
-    this.stockageSVG.setSVGEnCours(crayon);
+      crayon += 'L' + souris.offsetX + ' ' + souris.offsetY + ' "/>';
+      this.stockageSVG.setSVGEnCours(crayon);
+    }
   }
 
   sourisEnfoncee(souris: MouseEvent) {
+    this.traitEnCours = true;
     this.stockageSVG.setSVGEnCours(
       '<path filter="url(#' + this.outils.outilActif.parametres[1].optionChoisie
-      + ')"  fill="transparent" stroke="black" stroke-linecap="round" stroke-width="'
+      + ')" fill="transparent" stroke="black" stroke-linecap="round" stroke-width="'
       + this.outils.outilActif.parametres[0].valeur + '" d="M' + souris.offsetX + ' ' + souris.offsetY + '"/>');
   }
 
   sourisRelachee(souris: MouseEvent) {
-    const SVG: string = this.stockageSVG.getSVGEnCours();
-    if (SVG.includes('L')) {
-      /* on ne stocke le path que s'il n'y a au moins une ligne */
-      this.stockageSVG.ajouterSVG(this.stockageSVG.getSVGEnCours() + '" />');
-      this.stockageSVG.setSVGEnCours('');
+    if (this.traitEnCours) {
+      const SVG: string = this.stockageSVG.getSVGEnCours();
+      if (SVG.includes('L')) {
+        /* on ne stocke le path que s'il n'y a au moins une ligne */
+        this.stockageSVG.ajouterSVG(this.stockageSVG.getSVGEnCours() + '" />');
+        this.stockageSVG.setSVGEnCours('');
+      }
+      this.traitEnCours = false;
+      this.click = true;
     }
-    this.stockageSVG.setSVGEnCours('');
+  }
+
+  sourisCliquee(souris: MouseEvent) {
+    console.log('POINT PINCEAU');
+    if (this.click) {
+      if (this.outils.outilActif.parametres[0].valeur) {
+        const SVG = '<circle filter="url(#' + this.outils.outilActif.parametres[1].optionChoisie
+        + ')"  cx="' + souris.offsetX + '" cy="' + souris.offsetY + '" r="'
+        + this.outils.outilActif.parametres[0].valeur / 2 + '" fill="black"/>';
+        this.stockageSVG.ajouterSVG(SVG);
+      }
+      this.traitEnCours = false;
+    } else {this.click = true};
   }
 
   sourisSortie(souris: MouseEvent) {
-    this.stockageSVG.ajouterSVG(this.stockageSVG.getSVGEnCours() + '"/>');
-    this.stockageSVG.setSVGEnCours('');
+    if (this.traitEnCours) {
+      this.stockageSVG.ajouterSVG(this.stockageSVG.getSVGEnCours() + '"/>');
+      this.stockageSVG.setSVGEnCours('');
+      this.traitEnCours = false;
+    }
+    this.click = false;
   }
 }
