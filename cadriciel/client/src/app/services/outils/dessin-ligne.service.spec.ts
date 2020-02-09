e67d6bb446aeba3df68b89d02466dee8421e341f
Scandium <samuel.couvrette@polymtl.ca>
2020-02-09 14:52:38 -0500

    correction test de sourisDeplacee de dessin-ligne

--- cadriciel/client/src/app/services/outils/dessin-ligne.service.spec.ts
+++ cadriciel/client/src/app/services/outils/dessin-ligne.service.spec.ts
@@ -1,19 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DessinLigneService } from './dessin-ligne.service';
+import { INDEX_OUTIL_LIGNE } from './gestionnaire-outils.service';
 
 describe('DessinLigneService', () => {
   let service: DessinLigneService;
   beforeEach(() => TestBed.configureTestingModule({}));
   beforeEach(() => service = TestBed.get(DessinLigneService));
 
-  // Mettre l'outil pinceau comme l'outil actif
   beforeEach(() => {
-    //service.estClicSimple = true;
+    // service.estClicSimple = true;
     service.curseurX = 0;
     service.curseurY = 0;
     service.positionX = 0;
     service.positionY = 0;
+    // Mettre l'outil ligne comme l'outil actif
+    service.outils.outilActif = service.outils.listeOutils[INDEX_OUTIL_LIGNE];
   });
 
   it('should be created', () => {
@@ -24,10 +26,10 @@ describe('DessinLigneService', () => {
   // TESTS sourisDeplacee
 
   it('#sourisDeplacee devrait changer la position en x et en y', () => {
-    const clic = new MouseEvent('mousemove', { clientX: 100, clientY: 100 });/////////////////////////////////
+    const clic = new MouseEvent('mousemove', { clientX: 100, clientY: 100 });
     service.sourisDeplacee(clic);
-    expect(service.positionX).toBe(100);
-    expect(service.positionY).toBe(100);
+    expect(service.curseurX).toBe(100);
+    expect(service.curseurY).toBe(100);
   });
 
   it('#sourisDeplacee devrait appeler shiftEnfonce si shift est enfoncé', () => {
@@ -46,23 +48,23 @@ describe('DessinLigneService', () => {
     expect(service.shiftRelache).toHaveBeenCalled();
   });
 
-  // TESTS sourisClique
+  // TESTS sourisCliquee
 
-  it('#sourisClique devrait rajouter x et y au conteneur Point', () => {
+  it('#sourisCliquee devrait rajouter x et y au conteneur Point', () => {
     service.points = [];
     const clic = new MouseEvent('click', { clientX: 100, clientY: 100 });
     service.sourisCliquee(clic);
     expect(service.sourisCliquee).not.toBeNull();
   });
 
-  it('#sourisClique devrait appeler setTimeout de window', () => {
+  it('#sourisCliquee devrait appeler setTimeout de window', () => {
     spyOn(window, 'setTimeout');
     const clic = new MouseEvent('click', { clientX: 100, clientY: 100 }); //////////////////////////
     service.sourisCliquee(clic);
     expect(window.setTimeout).toHaveBeenCalled();
   });
 
-  it('#sourisClique devrait appeler actualiserSVG si setTimeout est appelé', () => {
+  it('#sourisCliquee devrait appeler actualiserSVG si setTimeout est appelé', () => {
     spyOn(service, 'actualiserSVG');
     const clic = new MouseEvent('click', { clientX: 100, clientY: 100 });
     service.sourisCliquee(clic);
