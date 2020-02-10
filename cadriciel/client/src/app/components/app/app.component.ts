6d42e2b9d09afa96322ea6a5719f78b5c4dfef51
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@polymtl.ca>
2020-02-10 11:07:31 -0500

    service index commente

--- cadriciel/client/src/app/components/app/app.component.ts
+++ cadriciel/client/src/app/components/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Router, RoutesRecognized } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { filter, map, pairwise } from 'rxjs/operators';
+import { filter, /*map,*/ pairwise } from 'rxjs/operators';
 import { GestionnaireRoutingService } from 'src/app/services/gestionnaire-routing.service';
-import { Message } from '../../../../../common/communication/message';
-import { IndexService } from '../../services/index/index.service';
+// import { Message } from '../../../../../common/communication/message';
+// import { IndexService } from '../../services/index/index.service';
 
 @Component({
     selector: 'app-root',
@@ -15,13 +15,13 @@ export class AppComponent {
     readonly title: string = 'LOG2990';
     message = new BehaviorSubject<string>('');
 
-    constructor(private basicService: IndexService,
+    constructor(// private basicService: IndexService,
                 private routing: Router,
                 public gestionnaireRoutes: GestionnaireRoutingService) {
-        this.basicService
-            .basicGet()
-            .pipe(map((message: Message) => `${message.title} ${message.body}`))
-            .subscribe(this.message);
+        // this.basicService
+            // .basicGet()
+            // .pipe(map((message: Message) => `${message.title} ${message.body}`))
+            // .subscribe(this.message);
         this.routing.events
             .pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
             .subscribe({next: (evenement) => {
