2ee963e598133d3e396a71cf70e08d068d719bb1
Symon <symon.nestoruk@polymtl.ca>
2020-02-10 13:10:16 -0500

    erreur?

--- cadriciel/client/src/app/components/accueil/accueil.component.spec.ts
+++ cadriciel/client/src/app/components/accueil/accueil.component.spec.ts
@@ -1,9 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material';
+import { MatDialog, MatDialogModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { BarreOutilsComponent } from '../barre-outils/barre-outils.component';
-import { FenetreNewDessinComponent } from '../fenetre-new-dessin/fenetre-new-dessin.component';
 import { GuideSujetComponent } from '../guide-sujet/guide-sujet.component';
 import { OutilDessinComponent } from '../outil-dessin/outil-dessin.component';
 import { PageDessinComponent } from '../page-dessin/page-dessin.component';
@@ -14,9 +13,6 @@ import { AccueilComponent } from './accueil.component';
 describe('AccueilComponent', () => {
   let component: AccueilComponent;
   let fixture: ComponentFixture<AccueilComponent>;
-  let dialog: MatDialog;
-  beforeEach(() => TestBed.configureTestingModule({}));
-  beforeEach(() => component = TestBed.get(AccueilComponent));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,22 +30,11 @@ describe('AccueilComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AccueilComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    const componentTest: AccueilComponent = TestBed.get(AccueilComponent);
-    expect(componentTest).toBeTruthy();
+    expect(component).toBeTruthy();
   });
-
-  it('creationDessin devrait  appeler dialog.open() avec les bons parametres', () => {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = '60%';
-    spyOn(dialog, 'open');
-    component.creationDessin();
-    expect(dialog.open).toHaveBeenCalledWith(FenetreNewDessinComponent, dialogConfig);
-  })
-
 });
