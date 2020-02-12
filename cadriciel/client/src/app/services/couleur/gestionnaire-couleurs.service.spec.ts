63b20996fee1dd0465c8ed51dc54e6145de54963
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@polymtl.ca>
2020-02-12 16:13:03 -0500

    debut test gestionnaire et parametre couleur

--- cadriciel/client/src/app/services/couleur/gestionnaire-couleurs.service.spec.ts
+++ cadriciel/client/src/app/services/couleur/gestionnaire-couleurs.service.spec.ts
@@ -3,10 +3,24 @@ import { TestBed } from '@angular/core/testing';
 import { GestionnaireCouleursService } from './gestionnaire-couleurs.service';
 
 describe('GestionnaireCouleursService', () => {
+  let service: GestionnaireCouleursService;
+
   beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => service = TestBed.get(GestionnaireCouleursService));
 
   it('should be created', () => {
-    const service: GestionnaireCouleursService = TestBed.get(GestionnaireCouleursService);
-    expect(service).toBeTruthy();
+    const testService: GestionnaireCouleursService = TestBed.get(GestionnaireCouleursService);
+    expect(testService).toBeTruthy();
+  });
+
+  /* it('#getCouleur devrait retourner la couleur actuel avec une opacité de 1', () => {
+    expect(service.getCouleur).toBe('rgba(0, 0, 0, 1)');
+  }); */
+
+  it('#modifierRGB devrait modifier la couleur avec le nouveau RGB', () => {
+    service.RGB = [1, 2, 3];
+    service.modifierRGB();
+
+    expect(service.couleur).toBe('rgba(1, 2, 3, ');
   });
 });
--- cadriciel/client/src/app/services/couleur/parametres-couleur.service.spec.ts
+++ cadriciel/client/src/app/services/couleur/parametres-couleur.service.spec.ts
@@ -3,10 +3,30 @@ import { TestBed } from '@angular/core/testing';
 import { ParametresCouleurService } from './parametres-couleur.service';
 
 describe('ParametresCouleurService', () => {
+  let service: ParametresCouleurService;
+
   beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => service = TestBed.get(ParametresCouleurService));
 
   it('should be created', () => {
-    const service: ParametresCouleurService = TestBed.get(ParametresCouleurService);
-    expect(service).toBeTruthy();
+    const testService: ParametresCouleurService = TestBed.get(ParametresCouleurService);
+    expect(testService).toBeTruthy();
   });
+
+  // TEST intervertirCouleur
+
+  it('#intervetirCouleur devrait échanger la couleur principale et la couleur secondaire', () => {
+    const copieCouleurPrincipal = service.couleurPrincipale;
+    const copieCouleurSecondaire = service.couleurSecondaire;
+
+    service.intervertirCouleurs();
+
+    expect(service.couleurPrincipale).toBe(copieCouleurSecondaire);
+    expect(service.couleurSecondaire).toBe(copieCouleurPrincipal);
+  });
+
+  // TEST getCouleurPrincipale
+
+  // TEST getCouleurSecondaire
+
 });
