2bd7e133f1ee2d87e46f1365b4ccafc1445c92cc
loalea <louis.alepin@polymtl.ca>
2020-02-12 22:20:35 -0500

    correction de certains tests de barre-outils

--- cadriciel/client/src/app/components/barre-outils/barre-outils.component.spec.ts
+++ cadriciel/client/src/app/components/barre-outils/barre-outils.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 
+// import { GestionnaireCouleursService } from 'src/app/services/couleur/gestionnaire-couleurs.service';
+import { MatDialogModule } from '@angular/material';
+import { GestionnaireRaccourcisService } from 'src/app/services/gestionnaire-raccourcis.service';
 import { GestionnaireOutilsService, OutilDessin } from 'src/app/services/outils/gestionnaire-outils.service';
 import { GuideSujetComponent } from '../guide-sujet/guide-sujet.component';
 import { OutilDessinComponent } from '../outil-dessin/outil-dessin.component';
@@ -14,21 +17,32 @@ const outilTestActif: OutilDessin = {
   ID: 0,
   parametres: []
 };
+
 const outilTestInactif: OutilDessin = {
   nom: 'stubInactif',
   estActif: false,
   ID: 1,
   parametres: []
 };
+
+const rectangle: OutilDessin = {
+  nom: 'stubRectangle',
+  estActif: false,
+  ID: 2,
+  parametres: []
+};
+
 const GestionnaireOutilServiceStub: Partial<GestionnaireOutilsService> = {
   listeOutils: [
     outilTestActif,
-    outilTestInactif
+    outilTestInactif,
+    rectangle
   ],
   outilActif: outilTestActif
 }
 
 describe('BarreOutilsComponent', () => {
+  let raccourcis: GestionnaireRaccourcisService;
   let component: BarreOutilsComponent;
   let fixture: ComponentFixture<BarreOutilsComponent>;
   let service: GestionnaireOutilsService;
@@ -39,7 +53,7 @@ describe('BarreOutilsComponent', () => {
       providers: [ {provide: GestionnaireOutilsService, useValue: GestionnaireOutilServiceStub} ],
       imports: [ RouterModule.forRoot([
         {path: 'guide', component : PageGuideComponent}
-    ])]
+    ]), MatDialogModule]
     })
     .compileComponents();
   }));
@@ -50,24 +64,37 @@ describe('BarreOutilsComponent', () => {
     fixture.detectChanges();
   });
   beforeEach(() => {
-    service.listeOutils[0].estActif = true;
-    service.listeOutils[1].estActif = false;
+    service.listeOutils[0].estActif = true; // outil crayon
+    service.listeOutils[1].estActif = false; // outil pinceau
+    service.listeOutils[2].estActif = false; // outil rectangle
+
+    /*raccourcis.outils = {outilActif: service.listeOutils[0], trouverIndexParametre: service.trouverIndexParametre,
+      listeOutils: service.listeOutils, changerOutilActif: service.changerOutilActif};
+    raccourcis.dessinRectangle = {rectangleEnCours: raccourcis.dessinRectangle.rectangleEnCours,
+      initial: ({x: 0, y: 0}), base: ({x: 0, y: 0}), baseCalculee: ({x: 0, y: 0}), largeur: 0, hauteur: 0, largeurCalculee: 0,
+    hauteurCalculee: 0, stockageSVG: 0, }
+    raccourcis.dessinLigne*/
   })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  // TESTS onClick
+
   it('#onClick devrait changer d\'outil', () => {
-    // on lui demande de changer à l'outil 2
-    component.onClick(service.listeOutils[1]);
-    // on vérifie que l'outil actif est bien le deuxième
-    expect(service.outilActif).toBe(service.listeOutils[1]);
+    component.onClick(service.listeOutils[1]); // on sélectionne l'outil 2
+    expect(service.outilActif).toBe(service.listeOutils[1]); // on vérifie que l'outil actif est bien le deuxième
   });
+
   it('#onClick devrait mettre le nouvel outil sélectionné comme actif', () => {
-    // on lui demande de changer à l'outil 2
-    component.onClick(service.listeOutils[1]);
-    // on vérifie que le nouvel outil est bien "actif"
-    expect(service.listeOutils[1].estActif).toBe(true);
-  })
+    component.onClick(service.listeOutils[1]); // on sélectionne l'outil 2
+    expect(service.listeOutils[1].estActif).toBe(true); // on vérifie que le nouvel outil est bien "actif"
+  });
+
+  it('#onClick devrait appeler la fonction viderSVGEnCours', () => {
+    spyOn(raccourcis, 'viderSVGEnCours');
+    component.onClick(service.listeOutils[2]); // on sélectionne l'outil 2
+    expect(raccourcis.viderSVGEnCours).toHaveBeenCalled();
+  });
 });
