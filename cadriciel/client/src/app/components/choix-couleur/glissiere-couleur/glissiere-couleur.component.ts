d60f9c8d7fe00bea21b87f9adcdd15fc7764b52f
Hugo Vandenbroucke-Menu <hugo.vandenbroucke-menu@polymtl.ca>
2020-02-13 11:23:36 -0500

    ajout de ; a fin des lignes pour components couleurs

--- cadriciel/client/src/app/components/choix-couleur/glissiere-couleur/glissiere-couleur.component.ts
+++ cadriciel/client/src/app/components/choix-couleur/glissiere-couleur/glissiere-couleur.component.ts
@@ -27,61 +27,61 @@ export class GlissiereCouleurComponent implements AfterViewInit, InterfaceOutils
   draw() {
 
     if (!this.context2D) {
-      this.context2D = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D
+      this.context2D = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
     }
-    const width = this.canvas.nativeElement.width
-    const height = this.canvas.nativeElement.height
+    const width = this.canvas.nativeElement.width;
+    const height = this.canvas.nativeElement.height;
 
-    this.context2D.clearRect(0, 0, width, height)
+    this.context2D.clearRect(0, 0, width, height);
 
-    const gradient = this.context2D.createLinearGradient(0, 0, 0, height)
-    gradient.addColorStop(0, 'rgba(255, 0, 0, 1)')
-    gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)')
-    gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)')
-    gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)')
-    gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)')
-    gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)')
-    gradient.addColorStop(1, 'rgba(255, 0, 0, 1)')
+    const gradient = this.context2D.createLinearGradient(0, 0, 0, height);
+    gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
+    gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
+    gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
+    gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
+    gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
+    gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
+    gradient.addColorStop(1, 'rgba(255, 0, 0, 1)');
 
-    this.context2D.beginPath()
-    this.context2D.rect(0, 0, width, height)
+    this.context2D.beginPath();
+    this.context2D.rect(0, 0, width, height);
 
-    this.context2D.fillStyle = gradient
-    this.context2D.fill()
-    this.context2D.closePath()
+    this.context2D.fillStyle = gradient;
+    this.context2D.fill();
+    this.context2D.closePath();
 
     if (this.hauteurChoisi) {
-      this.context2D.beginPath()
-      this.context2D.strokeStyle = 'white'
-      this.context2D.lineWidth = 5
-      this.context2D.rect(0, this.hauteurChoisi - 5, width, 10)
-      this.context2D.stroke()
-      this.context2D.closePath()
+      this.context2D.beginPath();
+      this.context2D.strokeStyle = 'white';
+      this.context2D.lineWidth = 5;
+      this.context2D.rect(0, this.hauteurChoisi - 5, width, 10);
+      this.context2D.stroke();
+      this.context2D.closePath();
     }
   }
 
   @HostListener('window:mouseup', ['$event'])
   sourisRelachee(evt: MouseEvent) {
-    this.sourisbas = false
+    this.sourisbas = false;
   }
 
   sourisEnfoncee(evt: MouseEvent) {
-    this.sourisbas = true
-    this.hauteurChoisi = evt.offsetY
-    this.draw()
-    this.couleurEmise(evt.offsetX, evt.offsetY)
+    this.sourisbas = true;
+    this.hauteurChoisi = evt.offsetY;
+    this.draw();
+    this.couleurEmise(evt.offsetX, evt.offsetY);
   }
 
   sourisDeplacee(evt: MouseEvent) {
     if (this.sourisbas) {
-      this.hauteurChoisi = evt.offsetY
-      this.draw()
-      this.couleurEmise(evt.offsetX, evt.offsetY)
+      this.hauteurChoisi = evt.offsetY;
+      this.draw();
+      this.couleurEmise(evt.offsetX, evt.offsetY);
     }
   }
 
   couleurEmise(x: number, y: number) {
-    this.couleurPosition(x, y)
+    this.couleurPosition(x, y);
   }
 
   couleurPosition(x: number, y: number) {
