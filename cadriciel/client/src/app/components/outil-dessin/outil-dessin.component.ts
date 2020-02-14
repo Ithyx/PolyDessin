43833f486dd08240b47169ccdc8d9f90dc63b807
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-02-14 16:58:24 -0500

    fix linting

--- cadriciel/client/src/app/components/outil-dessin/outil-dessin.component.ts
+++ cadriciel/client/src/app/components/outil-dessin/outil-dessin.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 
-// L'extension VS de TSLint montre cet ordre comme érroné, mais le programme TSLint de node en CLI nous montre que cet ordre là est correct
-import { OutilDessin, OUTIL_VIDE } from 'src/app/services/outils/gestionnaire-outils.service';
+import { OUTIL_VIDE, OutilDessin } from 'src/app/services/outils/gestionnaire-outils.service';
 
 @Component({
   selector: 'app-outil-dessin',
