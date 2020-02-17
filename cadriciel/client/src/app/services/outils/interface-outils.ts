c5b7f7c29d1736dedabbc201589e30dacbff0f28
H. V <hugo.vandenbroucke-menu@polymtl.ca>
2020-02-17 16:03:22 -0500

    il est possible de cliquer sur un element SVG

--- cadriciel/client/src/app/services/outils/interface-outils.ts
+++ cadriciel/client/src/app/services/outils/interface-outils.ts
@@ -2,7 +2,7 @@ export interface InterfaceOutils {
     sourisDeplacee?(evenement: MouseEvent): void;
     sourisEnfoncee?(evenement: MouseEvent): void;
     sourisRelachee?(evenement?: MouseEvent): void;
-    sourisCliquee?(evenement?: MouseEvent): void;
+    sourisCliquee?(evenement?: MouseEvent, cle?: number): void;
     sourisSortie?(evenement?: MouseEvent): void;
     sourisEntree?(evenement: MouseEvent): void;
     sourisDoubleClic?(evenement: MouseEvent): void;
--- cadriciel/client/src/app/services/outils/selection.service.ts
+++ cadriciel/client/src/app/services/outils/selection.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { StockageSvgService } from '../stockage-svg.service';
+import { GestionnaireOutilsService, INDEX_OUTIL_SELECTION } from './gestionnaire-outils.service';
 import { InterfaceOutils } from './interface-outils';
 
 @Injectable({
@@ -6,5 +8,16 @@ import { InterfaceOutils } from './interface-outils';
 })
 export class SelectionService implements InterfaceOutils {
 
-  constructor() { }
+  constructor(public stockageSVG: StockageSvgService,
+              public outils: GestionnaireOutilsService,
+             ) { }
+
+  sourisCliquee(evenement: MouseEvent, cle: number) {
+    if (this.outils.outilActif.ID === INDEX_OUTIL_SELECTION) {
+      console.log('selection', evenement, cle);
+
+      this.stockageSVG.getSVGComplets().get(cle);
+    }
+  }
+
 }
