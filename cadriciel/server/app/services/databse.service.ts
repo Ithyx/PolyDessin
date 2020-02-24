879ccbb9cd51b4d4d9d046185f898db06fd11764
Timothy <timothy.chheang@polymtl.ca>
2020-02-24 15:59:47 -0500

    commencement serveur

--- cadriciel/server/app/services/databse.service.ts
+++ cadriciel/server/app/services/databse.service.ts
@@ -1,27 +1,23 @@
 import { injectable } from 'inversify';
 import { Collection, FilterQuery, FindOneOptions, MongoClient, MongoClientOptions, UpdateQuery, } from 'mongodb';
+import { ElementDessin } from '../../../common/communication/element-dessin';
 
-const DATABASE_URL = 'mongodb+srv://PolyDessin:log2990@polydessin-zhlk9.mongodb.net/test?retryWrites=true&w=majority ';
+
+const DATABASE_URL = 'mongodb+srv://PolyDessin:log2990@polydessin-zhlk9.mongodb.net/test?retryWrites=true&w=majority';
 const DATABASE_NAME = 'polydessinDB';
 const DATABASE_COLLECTION = 'dessin';
-
-export interface Test1 {
-    name: string;
-    id?: number;
-}
 
 @injectable()
 export class DatabaseService {
 
-    collection: Collection<Test1>;
+    collection: Collection<ElementDessin>;
 
     private options: MongoClientOptions = {
         useNewUrlParser : true,
         useUnifiedTopology : true
     };
 
-
-    constructor(){
+    constructor() {
         MongoClient.connect(DATABASE_URL, this.options)
         .then((client: MongoClient) => {
             this.collection = client.db(DATABASE_NAME).collection(DATABASE_COLLECTION);
@@ -32,7 +28,4 @@ export class DatabaseService {
         });
     }
 
-    SendData() {
-        this.collection.insertOne({name: 'Tim'})
-    }
 }
