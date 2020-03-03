bed8b96270a654d3f7e457b86d484c73f299c27e
Scandium <samuel.couvrette@polymtl.ca>
2020-03-03 17:07:00 -0500

    debut des tests pour l'outil de pipette

--- cadriciel/client/src/app/services/tools/pipette-tool.service.spec.ts
+++ cadriciel/client/src/app/services/tools/pipette-tool.service.spec.ts
@@ -1,12 +1,56 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Scope } from '../color/color-manager.service';
 import { PipetteToolService } from './pipette-tool.service';
 
 describe('PipetteToolService', () => {
+  let service: PipetteToolService;
+  let canvas: HTMLCanvasElement;
+
   beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    canvas = document.createElement('canvas');
+    spyOn(document, 'querySelector').and.callFake(() => {
+      return canvas;
+    });
+    service = TestBed.get(PipetteToolService);
+  });
 
   it('should be created', () => {
-    const service: PipetteToolService = TestBed.get(PipetteToolService);
     expect(service).toBeTruthy();
+  });
+
+  // TESTS DE onMouseClick ET onRightClick
+
+  it('#onMouseClick devrait appeler pickColor avec le scope Primary', () => {
+    spyOn(service, 'pickColor');
+    const event = new MouseEvent('click');
+    service.onMouseClick(event);
+    expect(service.pickColor).toHaveBeenCalledWith(event, Scope.Primary);
+  });
+
+  it('#onRightClick devrait appeler pickColor avec le scope Secondary', () => {
+    spyOn(service, 'pickColor');
+    const event = new MouseEvent('contextmenu');
+    service.onRightClick(event);
+    expect(service.pickColor).toHaveBeenCalledWith(event, Scope.Secondary);
+  });
+
+  // TESTS DE pickColor
+
+  it('#pickColor devrait aller chercher le SVG du dessin', () => {
+    service.pickColor(new MouseEvent('click'), Scope.Default);
+    expect(document.querySelector).toHaveBeenCalledWith('.drawing');
+  });
+
+  it('#pickColor devrait aller chercher le canvas', () => {
+    service.pickColor(new MouseEvent('click'), Scope.Default);
+    expect(document.querySelector).toHaveBeenCalledWith('.canvas');
+  });
+
+  it('#pickColor devrait aller chercher le contexte du canvas', () => {
+    spyOn(canvas, 'getContext');
+    service.pickColor(new MouseEvent('click'), Scope.Default);
+    expect(canvas.getContext).toHaveBeenCalledWith('2d');
   });
 });
