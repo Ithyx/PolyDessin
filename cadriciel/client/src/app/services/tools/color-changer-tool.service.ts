1ad4f0495401596091b85acaa3d1876da4490fe1
H. V <hugo.vandenbroucke-menu@polymtl.ca>
2020-03-06 13:21:53 -0500

    correction TSLint

--- cadriciel/client/src/app/services/tools/color-changer-tool.service.ts
+++ cadriciel/client/src/app/services/tools/color-changer-tool.service.ts
@@ -3,36 +3,38 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { ColorParameterService } from '../color/color-parameter.service';
 import { DrawElement } from '../stockage-svg/draw-element';
 import { SVGStockageService } from '../stockage-svg/svg-stockage.service';
-// import { ToolInterface } from './tool-interface';
+import { ToolInterface } from './tool-interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ColorChangerToolService /*implements ToolInterface*/ {
+export class ColorChangerToolService implements ToolInterface {
 
   activeElement: DrawElement;
   activeElementID: number;
 
   constructor(public colorParameter: ColorParameterService,
-              public SVGStockage: SVGStockageService,
+              public stockageSVG: SVGStockageService,
               private sanitizer: DomSanitizer
               ) {
-                // this.activeElementID = 0;
+                this.activeElementID = 0;
               }
 
   onMouseClick(): void {
-    if (this.SVGStockage.getCompleteSVG()[this.activeElementID].primaryColor) {
-      this.SVGStockage.getCompleteSVG()[this.activeElementID].primaryColor = this.colorParameter.getPrimaryColor();
-      this.SVGStockage.getCompleteSVG()[this.activeElementID].draw();
-      this.SVGStockage.getCompleteSVG()[this.activeElementID].SVGHtml = this.sanitizer.bypassSecurityTrustHtml(this.SVGStockage.getCompleteSVG()[this.activeElementID].SVG);
+    if (this.stockageSVG.getCompleteSVG()[this.activeElementID].primaryColor) {
+      this.stockageSVG.getCompleteSVG()[this.activeElementID].primaryColor = this.colorParameter.getPrimaryColor();
+      this.stockageSVG.getCompleteSVG()[this.activeElementID].draw();
+      this.stockageSVG.getCompleteSVG()[this.activeElementID].SVGHtml =
+                  this.sanitizer.bypassSecurityTrustHtml(this.stockageSVG.getCompleteSVG()[this.activeElementID].SVG);
     }
   }
 
   onRightClick(): void {
-    if (this.SVGStockage.getCompleteSVG()[this.activeElementID].secondaryColor) {
-      this.SVGStockage.getCompleteSVG()[this.activeElementID].secondaryColor = this.colorParameter.getSecondaryColor();
-      this.SVGStockage.getCompleteSVG()[this.activeElementID].draw();
-      this.SVGStockage.getCompleteSVG()[this.activeElementID].SVGHtml = this.sanitizer.bypassSecurityTrustHtml(this.SVGStockage.getCompleteSVG()[this.activeElementID].SVG);
+    if (this.stockageSVG.getCompleteSVG()[this.activeElementID].secondaryColor) {
+      this.stockageSVG.getCompleteSVG()[this.activeElementID].secondaryColor = this.colorParameter.getSecondaryColor();
+      this.stockageSVG.getCompleteSVG()[this.activeElementID].draw();
+      this.stockageSVG.getCompleteSVG()[this.activeElementID].SVGHtml =
+                  this.sanitizer.bypassSecurityTrustHtml(this.stockageSVG.getCompleteSVG()[this.activeElementID].SVG);
     }
   }
 }
