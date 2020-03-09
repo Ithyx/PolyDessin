9e9f4ee890e730749d882ed1ed209dcad8f89f23
Louis Alepin <louis.alepin@polymtl.ca>
2020-03-09 16:34:33 -0400

    continuation d'efface

--- cadriciel/client/src/app/services/tools/eraser-tool.service.ts
+++ cadriciel/client/src/app/services/tools/eraser-tool.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { CommandManagerService } from '../command/command-manager.service';
 import { RectangleService } from '../stockage-svg/rectangle.service';
 import { Point } from './line-tool.service';
 import { ToolInterface } from './tool-interface';
 import { ToolManagerService } from './tool-manager.service';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -14,27 +14,25 @@ export class EraserToolService implements ToolInterface {
   mousePosition: Point = {x: 0, y: 0};
   thickness: number;
 
-
   constructor(public tools: ToolManagerService,
               private sanitizer: DomSanitizer,
               public commands: CommandManagerService) {
 
-    this.thickness = 0;
+    this.thickness = 20;
     this.eraser = new RectangleService();
   }
 
   makeSquare(): void {
-    this.eraser.SVG = '<rect x="' + this.mousePosition.x + '" y="' + this.mousePosition.y +
+    this.eraser.svg = '<rect x="' + this.mousePosition.x + '" y="' + this.mousePosition.y +
     '" width="' + this.thickness + '" height="' + this.thickness +
     '" stroke="rgba(0, 0, 0, 1)" fill="white" stroke-width="1"/>';
-    this.eraser.SVGHtml = this.sanitizer.bypassSecurityTrustHtml(this.eraser.SVG);
+    this.eraser.svgHtml = this.sanitizer.bypassSecurityTrustHtml(this.eraser.svg);
+    console.log(this.eraser.svgHtml);
   }
 
   onMouseMove(mouse: MouseEvent): void {
-    if (this.commands.drawingInProgress) {
-      this.mousePosition = {x: mouse.offsetX, y: mouse.offsetY};
-      this.makeSquare();
-    }
+    this.mousePosition = {x: mouse.offsetX, y: mouse.offsetY};
+    this.makeSquare();
   }
 
   onMousePress(mouse: MouseEvent): void {
