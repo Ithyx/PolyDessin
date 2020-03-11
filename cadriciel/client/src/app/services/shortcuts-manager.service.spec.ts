e668ce63512a750358ed9638e72939fc7b841c2f
H. V <hugo.vandenbroucke-menu@polymtl.ca>
2020-03-10 21:07:46 -0400

    correction des tests shortcut-manager et passage des attributs en prives

--- cadriciel/client/src/app/services/shortcuts-manager.service.spec.ts
+++ cadriciel/client/src/app/services/shortcuts-manager.service.spec.ts
@@ -1,9 +1,17 @@
-/*import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material';
 import { ShortcutsManagerService } from './shortcuts-manager.service';
 import { TOOL_INDEX } from './tools/tool-manager.service';
 
 describe('shortcuts-manager', () => {
+
   let service: ShortcutsManagerService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        imports: [MatDialogModule],
+    });
+}));
 
   beforeEach(() => TestBed.configureTestingModule({}));
   beforeEach(() => service = TestBed.get(ShortcutsManagerService));
@@ -22,28 +30,28 @@ describe('shortcuts-manager', () => {
   // TESTS clearOngoingSVG
 
   it('#clearOngoingSVG devrait detruire la boite de selection en cours', () => {
-    spyOn(service.selection, 'deleteBoundingBox');
+    spyOn(service['selection'], 'deleteBoundingBox');
     service.clearOngoingSVG();
-    expect(service.selection.deleteBoundingBox).toHaveBeenCalled();
+    expect(service['selection'].deleteBoundingBox).toHaveBeenCalled();
   });
 
   it('#clearOngoingSVG devrait vider le SVGEnCours de l\' outil rectange', () => {
-    spyOn(service.rectangleTool, 'clear');
+    spyOn(service['rectangleTool'], 'clear');
     service.clearOngoingSVG();
-    expect(service.rectangleTool.clear).toHaveBeenCalled();
+    expect(service['rectangleTool'].clear).toHaveBeenCalled();
   });
 
   it('#clearOngoingSVG devrait vider le SVGEnCours de l\' outil ligne', () => {
-    spyOn(service.lineTool, 'clear');
+    spyOn(service['lineTool'], 'clear');
     service.clearOngoingSVG();
-    expect(service.lineTool.clear).toHaveBeenCalled();
+    expect(service['lineTool'].clear).toHaveBeenCalled();
   });
 
   // TESTS shorcutKey1
 
   it('#shortcutKey1 devrait changer l\'outil actif pour le rectange', () => {
     service.shortcutKey1();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.RECTANGLE);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.RECTANGLE);
   });
 
   it('#shortcutKey1 devrait supprimer le SVG en cours', () => {
@@ -56,7 +64,7 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKey2 devrait changer l\'outil actif pour l\'ellipse', () => {
     service.shortcutKey2();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.ELLIPSE);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.ELLIPSE);
   });
 
   it('#shortcutKey2 devrait supprimer le SVG en cours', () => {
@@ -69,7 +77,7 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKey3 devrait changer l\'outil actif pour le polygone', () => {
     service.shortcutKey3();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.POLYGON);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.POLYGON);
   });
 
   it('#shortcutKey3 devrait supprimer le SVG en cours', () => {
@@ -83,34 +91,27 @@ describe('shortcuts-manager', () => {
   it('#shortcutKeyA devrait changer l\'outil actif pour l\'aérosol si CTRL est inactif', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'a' , ctrlKey: false});
     service.shortcutKeyA(keyboard);
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.SPRAY);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.SPRAY);
   });
 
   it('#shortcutKeyA devrait supprimer la boite de selection en cours', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'a' , ctrlKey: true});
-    spyOn(service.selection, 'deleteBoundingBox');
+    spyOn(service['selection'], 'deleteBoundingBox');
     service.shortcutKeyA(keyboard);
-    expect(service.selection.deleteBoundingBox).toHaveBeenCalled();
-  });
-
-  it('#shortcutKeyA devrait bloquer les raccourcis clavier du navigateur', () => {
-    const keyboard = new KeyboardEvent('keypress', { key: 'a' , ctrlKey: true});
-    spyOn(keyboard, 'preventDefault');
-    service.shortcutKeyA(keyboard);
-    expect(keyboard.preventDefault).toHaveBeenCalled();
+    expect(service['selection'].deleteBoundingBox).toHaveBeenCalled();
   });
 
   it('#shortcutKeyA devrait changer changer l\'outil actif pour la sélection', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'a' , ctrlKey: true});
     service.shortcutKeyA(keyboard);
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.SELECTION);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.SELECTION);
   });
 
   it('#shortcutKeyA ne devrait pas changer créer de boite de sélection si le nombre d\'SVG est nul', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'a' , ctrlKey: true});
-    spyOn(service.selection, 'createBoundingBox');
+    spyOn(service['selection'], 'createBoundingBox');
     service.shortcutKeyA(keyboard);
-    expect(service.selection.createBoundingBox).not.toHaveBeenCalled();
+    expect(service['selection'].createBoundingBox).not.toHaveBeenCalled();
   });
 
   // TODO : ShortcutKeyA dans le cas de plusieurs SVG
@@ -119,7 +120,7 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKeyC devrait changer l\'outil actif pour le crayon', () => {
     service.shortcutKeyC();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.PENCIL);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.PENCIL);
   });
 
   it('#shortcutKeyC devrait supprimer le SVG en cours', () => {
@@ -132,7 +133,7 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKeyI devrait changer l\'outil actif pour la pipette', () => {
     service.shortcutKeyI();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.PIPETTE);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.PIPETTE);
   });
 
   it('#shortcutKeyC devrait supprimer le SVG en cours', () => {
@@ -145,7 +146,7 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKeyL devrait changer l\'outil actif pour la ligne', () => {
     service.shortcutKeyL();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.LINE);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.LINE);
   });
 
   it('#shortcutKeyL devrait supprimer le SVG en cours', () => {
@@ -158,7 +159,7 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKeyW devrait changer l\'outil actif pour la ligne', () => {
     service.shortcutKeyW();
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.BRUSH);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.BRUSH);
   });
 
   it('#shortcutKeyW devrait supprimer le SVG en cours', () => {
@@ -172,7 +173,7 @@ describe('shortcuts-manager', () => {
   it('#shortcutKeyS devrait changer l\'outil actif pour la ligne', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 's' , ctrlKey: false});
     service.shortcutKeyS(keyboard);
-    expect(service.tools.activeTool.ID).toEqual(TOOL_INDEX.SELECTION);
+    expect(service['tools'].activeTool.ID).toEqual(TOOL_INDEX.SELECTION);
   });
 
   it('#shortcutKeyS devrait supprimer le SVG en cours', () => {
@@ -182,81 +183,82 @@ describe('shortcuts-manager', () => {
     expect(service.clearOngoingSVG).toHaveBeenCalled();
   });
 
+  
   // TESTS shortcutKeyZ
 
   it('#shortcutKeyZ devrait annuler la dernier commande si CTRL est actif et qu\'il n\'y a pas de dessin en cours', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'z' , ctrlKey: true});
-    service.commands.drawingInProgress = false;
-    spyOn(service.commands, 'cancelCommand');
+    service['commands'].drawingInProgress = false;
+    spyOn(service['commands'], 'cancelCommand');
     service.shortcutKeyZ(keyboard);
-    expect(service.commands.cancelCommand).toHaveBeenCalled();
+    expect(service['commands'].cancelCommand).toHaveBeenCalled();
   });
 
   it('#shortcutKeyZ ne devrait rien faire si CTRL n\'est pas actif', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'z' , ctrlKey: false});
-    service.commands.drawingInProgress = false;
-    spyOn(service.commands, 'cancelCommand');
+    service['commands'].drawingInProgress = false;
+    spyOn(service['commands'], 'cancelCommand');
     service.shortcutKeyZ(keyboard);
-    expect(service.commands.cancelCommand).not.toHaveBeenCalled();
+    expect(service['commands'].cancelCommand).not.toHaveBeenCalled();
   });
 
   it('#shortcutKeyZ ne devrait rien faire si un dessin est en cours', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'z' , ctrlKey: true});
-    service.commands.drawingInProgress = true;
-    spyOn(service.commands, 'cancelCommand');
+    service['commands'].drawingInProgress = true;
+    spyOn(service['commands'], 'cancelCommand');
     service.shortcutKeyZ(keyboard);
-    expect(service.commands.cancelCommand).not.toHaveBeenCalled();
+    expect(service['commands'].cancelCommand).not.toHaveBeenCalled();
   });
 
   // TESTS shortcutKeyUpperZ
 
   it('#shortcutKeyUpperZ devrait refaire la dernier commande si CTRL est actif et qu\'il n\'y a pas de dessin en cours', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'Z' , ctrlKey: true});
-    service.commands.drawingInProgress = false;
-    spyOn(service.commands, 'redoCommand');
+    service['commands'].drawingInProgress = false;
+    spyOn(service['commands'], 'redoCommand');
     service.shortcutKeyUpperZ(keyboard);
-    expect(service.commands.redoCommand).toHaveBeenCalled();
+    expect(service['commands'].redoCommand).toHaveBeenCalled();
   });
 
   it('#shortcutKeyUpperZ ne devrait rien faire si CTRL n\'est pas actif', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'Z' , ctrlKey: false});
-    service.commands.drawingInProgress = false;
-    spyOn(service.commands, 'redoCommand');
+    service['commands'].drawingInProgress = false;
+    spyOn(service['commands'], 'redoCommand');
     service.shortcutKeyUpperZ(keyboard);
-    expect(service.commands.redoCommand).not.toHaveBeenCalled();
+    expect(service['commands'].redoCommand).not.toHaveBeenCalled();
   });
 
   it('#shortcutKeyUpperZ ne devrait rien faire si un dessin est en cours', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'Z' , ctrlKey: true});
-    service.commands.drawingInProgress = true;
-    spyOn(service.commands, 'redoCommand');
+    service['commands'].drawingInProgress = true;
+    spyOn(service['commands'], 'redoCommand');
     service.shortcutKeyUpperZ(keyboard);
-    expect(service.commands.redoCommand).not.toHaveBeenCalled();
+    expect(service['commands'].redoCommand).not.toHaveBeenCalled();
   });
 
   // TESTS shortcutKeyShift
 
   it('#shortcutKeyShift devrait appeler shiftPress si l\'outil actif est le rectangle', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.RECTANGLE];
-    spyOn(service.rectangleTool, 'shiftPress');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.RECTANGLE];
+    spyOn(service['rectangleTool'], 'shiftPress');
     service.shortcutKeyShift();
-    expect(service.rectangleTool.shiftPress).toHaveBeenCalled();
+    expect(service['rectangleTool'].shiftPress).toHaveBeenCalled();
   });
 
   it('#shortcutKeyShift devrait memoriser la position du curseur si l\'outil actif est la ligne', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.LINE];
-    spyOn(service.lineTool, 'memorizeCursor');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.LINE];
+    spyOn(service['lineTool'], 'memorizeCursor');
     service.shortcutKeyShift();
-    expect(service.lineTool.memorizeCursor).toHaveBeenCalled();
+    expect(service['lineTool'].memorizeCursor).toHaveBeenCalled();
   });
 
   it('#shortcutKeyShift ne devrait rien faire si l\'outil actif n\'est ni la ligne ou le rectangle', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.PENCIL];
-    spyOn(service.lineTool, 'memorizeCursor');
-    spyOn(service.rectangleTool, 'shiftPress');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.PENCIL];
+    spyOn(service['lineTool'], 'memorizeCursor');
+    spyOn(service['rectangleTool'], 'shiftPress');
     service.shortcutKeyShift();
-    expect(service.lineTool.memorizeCursor).not.toHaveBeenCalled();
-    expect(service.rectangleTool.shiftPress).not.toHaveBeenCalled();
+    expect(service['lineTool'].memorizeCursor).not.toHaveBeenCalled();
+    expect(service['rectangleTool'].shiftPress).not.toHaveBeenCalled();
   });
 
   // TESTS shortcutKeyO
@@ -270,84 +272,77 @@ describe('shortcuts-manager', () => {
 
   it('#shortcutKeyO devrait detruire la boite de selection si CTRL est actif', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'o' , ctrlKey: true});
-    spyOn(service.selection, 'deleteBoundingBox');
+    spyOn(service['selection'], 'deleteBoundingBox');
     service.shortcutKeyO(keyboard);
-    expect(service.selection.deleteBoundingBox).toHaveBeenCalled();
-  });
-
-  it('#shortcutKeyO devrait bloquer les raccourcis du navigateur si CTRL est actif', () => {
-    const keyboard = new KeyboardEvent('keypress', { key: 'o' , ctrlKey: true});
-    spyOn(keyboard, 'preventDefault');
-    service.shortcutKeyO(keyboard);
-    expect(keyboard.preventDefault).toHaveBeenCalled();
+    expect(service['selection'].deleteBoundingBox).toHaveBeenCalled();
   });
 
   it('#shortcutKeyO ne devrait rien faire si CTRL est inactif', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'o' , ctrlKey: false});
     spyOn(service.newDrawingEmmiter, 'next');
-    spyOn(service.selection, 'deleteBoundingBox');
+    spyOn(service['selection'], 'deleteBoundingBox');
     spyOn(keyboard, 'preventDefault');
     service.shortcutKeyO(keyboard);
     expect(service.newDrawingEmmiter.next).not.toHaveBeenCalledWith(false);
-    expect(service.selection.deleteBoundingBox).not.toHaveBeenCalled();
+    expect(service['selection'].deleteBoundingBox).not.toHaveBeenCalled();
     expect(keyboard.preventDefault).not.toHaveBeenCalled();
   });
 
   // TESTS shortcutKeyBackSpace
 
   it('#shortcutKeyBackSpace devrait retirer le dernier si l\'outil acitf est la ligne', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.LINE];
-    spyOn(service.lineTool, 'removePoint');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.LINE];
+    spyOn(service['lineTool'], 'removePoint');
     service.shortcutKeyBackSpace();
-    expect(service.lineTool.removePoint).toHaveBeenCalled();
+    expect(service['lineTool'].removePoint).toHaveBeenCalled();
   });
 
   it('#shortcutKeyBackSpace ne devrait pas retirer le dernier si l\'outil acitf n\'est pas la ligne', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.PENCIL];
-    spyOn(service.lineTool, 'removePoint');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.PENCIL];
+    spyOn(service['lineTool'], 'removePoint');
     service.shortcutKeyBackSpace();
-    expect(service.lineTool.removePoint).not.toHaveBeenCalled();
+    expect(service['lineTool'].removePoint).not.toHaveBeenCalled();
   });
 
   // TESTS shortcutKeyEscape
 
   it('#shortcutKeyEscape devrait supprimer la ligne en cours si l\'outil acitf est la ligne', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.LINE];
-    spyOn(service.lineTool, 'clear');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.LINE];
+    spyOn(service['lineTool'], 'clear');
     service.shortcutKeyEscape();
-    expect(service.lineTool.clear).toHaveBeenCalled();
+    expect(service['lineTool'].clear).toHaveBeenCalled();
   });
 
   it('#shortcutKeyEscape ne devrait pas supprimer la ligne en cours si l\'outil acitf est la ligne', () => {
-    service.tools.activeTool = service.tools.toolList[TOOL_INDEX.PENCIL];
-    spyOn(service.lineTool, 'clear');
+    service['tools'].activeTool = service['tools'].toolList[TOOL_INDEX.PENCIL];
+    spyOn(service['lineTool'], 'clear');
     service.shortcutKeyEscape();
-    expect(service.lineTool.clear).not.toHaveBeenCalled();
+    expect(service['lineTool'].clear).not.toHaveBeenCalled();
   });
 
   // TESTS shortcutG
 
   it('#shortcutKeyG devrait inverser l\'etat d\'affichage de la grille', () => {
-    service.grid.showGrid = false;
+    service['grid'].showGrid = false;
     const keyboard = new KeyboardEvent('keypress', { key: 'g' , ctrlKey: false});
     service.shortcutKeyG(keyboard);
-    expect(service.grid.showGrid).toBe(true);
+    expect(service['grid'].showGrid).toBe(true);
   });
 
   // TESTS shortcutKeyPlus
 
   it('#shortcutKeyPlus devrait augmenter l\'espace de la grille', () => {
-    spyOn(service.grid, 'increaseSize');
+    spyOn(service['grid'], 'increaseSize');
     service.shortcutKeyPlus();
-    expect(service.grid.increaseSize).toHaveBeenCalled();
+    expect(service['grid'].increaseSize).toHaveBeenCalled();
   });
 
   // TESTS shortcutKeyMinus
 
   it('#shortcutKeyMinus devrait diminuer l\'espace de la grille', () => {
-    spyOn(service.grid, 'decreaseSize');
+    spyOn(service['grid'], 'decreaseSize');
     service.shortcutKeyMinus();
-    expect(service.grid.decreaseSize).toHaveBeenCalled();
+    expect(service['grid'].decreaseSize).toHaveBeenCalled();
   });
 
    // TESTS shortcutArrowLeft
@@ -386,22 +381,22 @@ describe('shortcuts-manager', () => {
 
   it('#treatReleaseKey devrait appeler shiftRelache de l\'outil rectangle si il reçoit Shift', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'Shift'});
-    service.tools.changeActiveTool(TOOL_INDEX.RECTANGLE);
-    spyOn(service.rectangleTool, 'shiftRelease');
+    service['tools'].changeActiveTool(TOOL_INDEX.RECTANGLE);
+    spyOn(service['rectangleTool'], 'shiftRelease');
 
     service.treatReleaseKey(keyboard);
 
-    expect(service.rectangleTool.shiftRelease).toHaveBeenCalled();
+    expect(service['rectangleTool'].shiftRelease).toHaveBeenCalled();
   });
 
   it('#treatReleaseKey devrait appeler shiftRelache de l\'outil ligne si il reçoit Shift', () => {
     const keyboard = new KeyboardEvent('keypress', { key: 'Shift'});
-    service.tools.changeActiveTool(TOOL_INDEX.LINE);
-    spyOn(service.lineTool, 'shiftRelease');
+    service['tools'].changeActiveTool(TOOL_INDEX.LINE);
+    spyOn(service['lineTool'], 'shiftRelease');
 
     service.treatReleaseKey(keyboard);
 
-    expect(service.lineTool.shiftRelease).toHaveBeenCalled();
+    expect(service['lineTool'].shiftRelease).toHaveBeenCalled();
   });
 
   it('#treatReleaseKey devrait mettre leftArrow a false si il reçoit ArrowLeft', () => {
@@ -436,18 +431,18 @@ describe('shortcuts-manager', () => {
     const keyboard = new KeyboardEvent('keypress');
 
     // Dans le cas de la ligne
-    service.tools.changeActiveTool(TOOL_INDEX.LINE);
-    spyOn(service.lineTool, 'shiftRelease');
+    service['tools'].changeActiveTool(TOOL_INDEX.LINE);
+    spyOn(service['lineTool'], 'shiftRelease');
     service.treatReleaseKey(keyboard);
-    expect(service.lineTool.shiftRelease).not.toHaveBeenCalled();
+    expect(service['lineTool'].shiftRelease).not.toHaveBeenCalled();
 
     // Dans le cas du rectangle
-    service.tools.changeActiveTool(TOOL_INDEX.RECTANGLE);
-    spyOn(service.rectangleTool, 'shiftRelease');
+    service['tools'].changeActiveTool(TOOL_INDEX.RECTANGLE);
+    spyOn(service['rectangleTool'], 'shiftRelease');
     service.treatReleaseKey(keyboard);
-    expect(service.rectangleTool.shiftRelease).not.toHaveBeenCalled();
+    expect(service['rectangleTool'].shiftRelease).not.toHaveBeenCalled();
 
   });
 
   // TODO: Désactiver limite fichier avec TSLINT
-});*/
+});
