db4f877599d0e760d7f25156e0a98d96a5d4291c
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-03-14 12:20:56 -0400

    fix nom database service du serveur

--- cadriciel/server/app/inversify.config.ts
+++ cadriciel/server/app/inversify.config.ts
@@ -2,7 +2,7 @@ import { Container } from 'inversify';
 import { Application } from './app';
 import { DatabaseController } from './controllers/database.controller';
 import { Server } from './server';
-import { DatabaseService } from './services/databse.service';
+import { DatabaseService } from './services/database.service';
 import Types from './types';
 
 const container: Container = new Container();
