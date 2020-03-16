16de7b13217d2acfbb111db0ee9d05409d603f16
Timothy <timothy.chheang@polymtl.ca>
2020-03-16 14:29:32 -0400

    erreur merge

--- cadriciel/server/app/services/database.service.spec.ts
+++ cadriciel/server/app/services/database.service.spec.ts
@@ -1,9 +1,16 @@
 import * as assert from 'assert';
 import {DatabaseService} from './databse.service';
 
-describe('Test constructeur', () =>{
+describe('Test constructeur', () => {
     it('constructeur doit etre bien defini', (done: Mocha.Done) => {
         assert.ok(new DatabaseService());
         done();
     });
 });
+
+/*describe('#updateData()', function() {
+    context('sans nom et doit exister', function(){
+        it('doit retourner faux', function(){
+        })
+    })
+}) */
