5c6a7eb3c5ee3f7b6123b4b473d04446f79cd65a
H. V <hugo.vandenbroucke-menu@polymtl.ca>
2020-03-30 15:06:43 -0400

    Ajout d'une constante pour la couleur de background

--- cadriciel/client/src/app/services/drawing-manager/drawing-manager.service.ts
+++ cadriciel/client/src/app/services/drawing-manager/drawing-manager.service.ts
@@ -6,6 +6,10 @@ const BUFFER_HEIGHT = 4;
 const INITIAL_HEIGHT = window.innerHeight - BUFFER_HEIGHT;
 const INITIAL_WIDTH = window.innerWidth - BUFFER_WIDTH;
 const RGB_MAX = 255;
+const BACKGROUND_COLOR_DEFAULT: Color = {
+  RGBAString: 'rgba(255, 255, 255, 1)',
+  RGBA: [RGB_MAX, RGB_MAX, RGB_MAX, 1]
+};
 
 @Injectable({
   providedIn: 'root'
@@ -24,10 +28,7 @@ export class DrawingManagerService {
     this.width = INITIAL_WIDTH;
     this.id = 0;
     this.name = '';
-    this.backgroundColor = {
-      RGBAString: 'rgba(255, 255, 255, 1)',
-      RGBA: [RGB_MAX, RGB_MAX, RGB_MAX, 1]
-    };
+    this.backgroundColor = BACKGROUND_COLOR_DEFAULT;
     this.tags = [];
   }
 }
