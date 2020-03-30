973ab4c6a9ee2ed4e88ae21046f40e9127ed4bfb
Timothy <timothy.chheang@polymtl.ca>
2020-03-30 11:25:14 -0400

    test server bon reste a mettre promise bool

--- cadriciel/server/app/services/database.service.spec.ts
+++ cadriciel/server/app/services/database.service.spec.ts
@@ -1,7 +1,8 @@
+// tslint:disable: no-magic-numbers
 import {assert, expect } from 'chai';
+import {Color, DrawElement} from '../../../client/src/app/services/stockage-svg/draw-element';
 import {Drawing} from '../../../common/communication/drawing-interface';
 import {DatabaseService} from './database.service';
-import {Color, DrawElement} from '../../../client/src/app/services/stockage-svg/draw-element';
 
 describe('Test constructeur database.service', () => {
     it('constructeur doit etre bien defini', (done: Mocha.Done) => {
@@ -10,39 +11,60 @@ describe('Test constructeur database.service', () => {
     });
 });
 
-/*describe('#updateData devrait retourner false, si le nom de la du dessin est vide ou collection non existant', () => {
+describe('#updateData devrait retourner false, si le nom de la du dessin est vide ou collection non existant', () => {
 
-    const red: Color = {
+    let black: Color;
+    beforeEach(() => black = {
         RGBA: [255, 255, 255, 1],
         RGBAString: ('ffffff'),
-    };
+    });
 
-    const drawing: Drawing = {
+    let element: DrawElement;
+    beforeEach(() => element = {
+        svg: '',
+        svgHtml: '',
+        points: [{x: 90, y: 90}, {x: 76, y: 89 }],
+        isSelected: false,
+        erasingEvidence: false,
+        erasingColor: {RGBA: [0, 0, 0, 1], RGBAString: ''},
+        pointMin: {x: 0, y: 0},
+        pointMax: {x: 0, y: 0},
+        translate: {x: 0, y: 0},
+        draw: () => { return; },
+        updatePosition: () => { return; },
+        updatePositionMouse: () => { return; },
+        updateParameters: () => { return; },
+        translateAllPoints: () => { return; }
+      }
+      );
+
+    let drawing: Drawing;
+    beforeEach(() => drawing = {
         _id: 123,
         name: '',
         height: 25,
         width: 25,
-        backgroundColor: red,
+        backgroundColor: black,
         tags: [''],
-        elements:,
-    };
+        elements: element[''],
+    });
 
     context('nom du dessin est vide', () => {
-        it('Doit retourner faux', async (done: Mocha.Done) => {
-            drawing.name('');
-            expect(name).to.be('');
-            done();
+        it('Doit retourner faux', async () => {
+            drawing.name = ('');
+            expect(drawing.name).to.equal('');
          });
     });
 
-    context('collection nexiste pas', () => {
+   /*context('collection nexiste pas', () => {
         it('Doit retourner faux', () => {
         });
-    });
+    }); */
 
-    context('nom du dessin nest pas vide', () =>{
-        it('Doit retourner vrai', () =>{
-
+    context('nom du dessin nest pas vide', () => {
+        it('Doit retourner vrai', async () => {
+            drawing.name = ('test1');
+            expect(drawing.name).to.equal('test1');
         });
     });
-}); */
+});
