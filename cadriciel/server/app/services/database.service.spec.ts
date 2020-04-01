50a3cf18a6c8a3701aac391506da13ec1155b3fd
Timothy <timothy.chheang@polymtl.ca>
2020-04-01 19:09:10 -0400

    continuation test database.service

--- cadriciel/server/app/services/database.service.spec.ts
+++ cadriciel/server/app/services/database.service.spec.ts
@@ -1,6 +1,8 @@
 // tslint:disable: no-magic-numbers
 import {assert, expect } from 'chai';
-import {Color, DrawElement} from '../../../client/src/app/services/stockage-svg/draw-element';
+import {Collection} from 'mongodb';
+import {Color} from '../../../client/src/app/services/color/color';
+import {DrawElement} from '../../../client/src/app/services/stockage-svg/draw-element';
 import {Drawing} from '../../../common/communication/drawing-interface';
 import {DatabaseService} from './database.service';
 
@@ -13,30 +15,31 @@ describe('Test constructeur database.service', () => {
 
 describe('#updateData devrait retourner false, si le nom de la du dessin est vide ou collection non existant', () => {
 
+    let collection: Collection<Drawing>;
+
     let black: Color;
     beforeEach(() => black = {
         RGBA: [255, 255, 255, 1],
         RGBAString: ('ffffff'),
     });
 
-    let element: DrawElement;
-    beforeEach(() => element = {
-        svg: '',
-        svgHtml: '',
-        points: [{x: 90, y: 90}, {x: 76, y: 89 }],
-        isSelected: false,
-        erasingEvidence: false,
-        erasingColor: {RGBA: [0, 0, 0, 1], RGBAString: ''},
-        pointMin: {x: 0, y: 0},
-        pointMax: {x: 0, y: 0},
-        translate: {x: 0, y: 0},
-        draw: () => { return; },
-        updatePosition: () => { return; },
-        updatePositionMouse: () => { return; },
-        updateParameters: () => { return; },
-        translateAllPoints: () => { return; }
-      }
-      );
+    const element: DrawElement = {
+    svg: '',
+    svgHtml: '',
+    trueType: 0,
+    points: [],
+    isSelected: false,
+    erasingEvidence: false,
+    erasingColor: {RGBA: [0, 0, 0, 0], RGBAString: ''},
+    pointMin: {x: 0, y: 0},
+    pointMax: {x: 0, y: 0},
+    translate: {x: 0, y: 0},
+    draw: () => { return; },
+    updatePosition: () => { return; },
+    updatePositionMouse: () => { return; },
+    updateParameters: () => { return; },
+    translateAllPoints: () => { return; }
+    };
 
     let drawing: Drawing;
     beforeEach(() => drawing = {
@@ -56,10 +59,12 @@ describe('#updateData devrait retourner false, si le nom de la du dessin est vid
          });
     });
 
-   /*context('collection nexiste pas', () => {
+    /*context('collection nexiste pas', () => {
         it('Doit retourner faux', () => {
+            collection.insertOne(drawing);
+            expect(collection).to.be.empty('');
         });
-    }); */
+    });*/
 
     context('nom du dessin nest pas vide', () => {
         it('Doit retourner vrai', async () => {
