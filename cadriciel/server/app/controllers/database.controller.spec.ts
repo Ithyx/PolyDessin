e61f23e0263413e7455a8777464bf3fc101fb398
H. V <hugo.vandenbroucke-menu@polymtl.ca>
2020-04-02 10:47:16 -0400

    test constructeur de database.controller

--- cadriciel/server/app/controllers/database.controller.spec.ts
+++ cadriciel/server/app/controllers/database.controller.spec.ts
@@ -1,9 +1,21 @@
 import * as assert from 'assert';
-import {DatabaseService} from '../services/database.service';
+import { DatabaseService } from '../services/database.service';
+import { DatabaseController } from './database.controller';
+
+// tslint:disable: no-string-literal
 
 describe('Test constructeur database.controller', () => {
     it('constructeur doit etre bien defini', (done: Mocha.Done) => {
         assert.ok(new DatabaseService());
         done();
     });
+
+    it('constructeur devrait appeler configureRouter', () => {
+        const controller: DatabaseController = new DatabaseController(new DatabaseService());
+        assert.call(controller, controller['configureRouter']);
+    });
+});
+
+describe('Tests configureRouter database.controller', () => {
+    // TODO
 });
