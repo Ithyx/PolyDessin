f539438ea0efedf40702528f814c2f1c88eeaf88
Timothy <timothy.chheang@polymtl.ca>
2020-04-01 23:27:53 -0400

    erreur de test sur updateData

--- cadriciel/server/app/services/database.service.spec.ts
+++ cadriciel/server/app/services/database.service.spec.ts
@@ -1,6 +1,5 @@
 // tslint:disable: no-magic-numbers
 import {assert, expect } from 'chai';
-import {Collection} from 'mongodb';
 import {Color} from '../../../client/src/app/services/color/color';
 import {DrawElement} from '../../../client/src/app/services/stockage-svg/draw-element';
 import {Drawing} from '../../../common/communication/drawing-interface';
@@ -13,8 +12,9 @@ describe('Test constructeur database.service', () => {
     });
 });
 
-describe('#updateData devrait retourner false, si le nom de la du dessin est vide ou collection non existant', () => {
+/*describe('#updateData devrait retourner false, si le nom de la du dessin est vide ou collection non existant', () => {
 
+    var test = ('../databaseService');
     let collection: Collection<Drawing>;
 
     let black: Color;
@@ -56,20 +56,66 @@ describe('#updateData devrait retourner false, si le nom de la du dessin est vid
         it('Doit retourner faux', async () => {
             drawing.name = ('');
             expect(drawing.name).to.equal('');
+            assert.equal(test(), 1);
          });
     });
 
-    /*context('collection nexiste pas', () => {
+    context('collection nexiste pas', () => {
         it('Doit retourner faux', () => {
             collection.insertOne(drawing);
             expect(collection).to.be.empty('');
         });
-    });*/
+    });
 
     context('nom du dessin nest pas vide', () => {
         it('Doit retourner vrai', async () => {
             drawing.name = ('test1');
             expect(drawing.name).to.equal('test1');
         });
+    });
+}); */
+
+describe('updateData', () => {
+
+    let test: DatabaseService;
+    let black: Color;
+    beforeEach(() => black = {
+        RGBA: [255, 255, 255, 1],
+        RGBAString: ('ffffff'),
+    });
+
+    const element: DrawElement = {
+        svg: '',
+        svgHtml: '',
+        trueType: 0,
+        points: [],
+        isSelected: false,
+        erasingEvidence: false,
+        erasingColor: {RGBA: [0, 0, 0, 0], RGBAString: ''},
+        pointMin: {x: 0, y: 0},
+        pointMax: {x: 0, y: 0},
+        translate: {x: 0, y: 0},
+        draw: () => { return; },
+        updatePosition: () => { return; },
+        updatePositionMouse: () => { return; },
+        updateParameters: () => { return; },
+        translateAllPoints: () => { return; }
+        };
+
+    let drawing: Drawing;
+    beforeEach(() => drawing = {
+        _id: 123,
+        name: '',
+        height: 25,
+        width: 25,
+        backgroundColor: black,
+        tags: [''],
+        elements: element[''],
+        });
+
+    it('Devrait retourner faux lorsque nom est vide', () => {
+        drawing.name = ('');
+        const returnfalse = test.updateData(drawing) ;
+        expect(returnfalse).to.equal(false);
     });
 });
