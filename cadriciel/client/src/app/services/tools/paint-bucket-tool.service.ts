2e023b51909358ec7318ad9b53b1e4977a2bd96d
Scandium <samuel.couvrette@polymtl.ca>
2020-04-05 12:03:30 -0400

    nette amelioration des performances de l'algorithme de remplissage (~20 secondes pour le dessin complet)

--- cadriciel/client/src/app/services/tools/paint-bucket-tool.service.ts
+++ cadriciel/client/src/app/services/tools/paint-bucket-tool.service.ts
@@ -7,10 +7,6 @@ import { TracePencilService } from '../stockage-svg/draw-element/trace/trace-pen
 import { SVGStockageService } from '../stockage-svg/svg-stockage.service';
 import { ToolInterface } from './tool-interface';
 
-// Constantes représentant les possibilités de points contigus au point courant
-const row = [ -1, -1, -1,  0,  0,  1,  1,  1 ];
-const col = [ -1,  0,  1, -1,  1, -1,  0,  1 ];
-
 const PENCIL_THICKNESS = 3;
 
 @Injectable({
@@ -24,7 +20,7 @@ export class PaintBucketToolService implements ToolInterface {
   private image: HTMLImageElement;
   private mousePosition: Point;
   private fill: TracePencilService;
-  private checkedPixels: number[];
+  private checkedPixels: number[]; // Map<number, number[]>;
   private color: [number, number, number];
 
   constructor(private colorParameter: ColorParameterService,
@@ -33,7 +29,7 @@ export class PaintBucketToolService implements ToolInterface {
     this.fill = new TracePencilService();
     this.color = [0, 0, 0];
     this.mousePosition = {x: 0, y: 0};
-    this.checkedPixels = [];
+    this.checkedPixels = []; // new Map<number, number[]>();
   }
 
   onMouseClick(mouse: MouseEvent): void {
@@ -42,7 +38,7 @@ export class PaintBucketToolService implements ToolInterface {
     this.fill.points = [];
     this.fill.primaryColor = this.colorParameter.primaryColor;
     this.fill.thickness = PENCIL_THICKNESS;
-    this.checkedPixels = [];
+    this.checkedPixels = []; // new Map<number, number[]>();
     this.createCanvas();
   }
 
@@ -55,16 +51,17 @@ export class PaintBucketToolService implements ToolInterface {
       const svgString = new XMLSerializer().serializeToString(this.drawing);
       const svg = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
       this.image = new Image();
-      this.image.onload = this.fillWithColor.bind(this);
+      this.image.onload = this.fillWithColor2.bind(this);
       this.image.src = URL.createObjectURL(svg);
     }
   }
 
-  // algorithme basé sur : https://www.techiedelight.com/flood-fill-algorithm/
-  fillWithColor(): void {
+  // http://www.programmersought.com/article/3670113928/
+  fillWithColor2(): void {
     this.context.drawImage(this.image, 0, 0);
     const pixelData = this.context.getImageData(this.mousePosition.x, this.mousePosition.y, 1, 1).data;
     this.color = [pixelData[0], pixelData[1], pixelData[2]];
+
     const queue: Point[] = [];
     queue.push(this.mousePosition);
 
@@ -72,21 +69,45 @@ export class PaintBucketToolService implements ToolInterface {
       const point = queue.pop();
       if (point) {
         this.checkedPixels.push(this.getIndex(point));
-        for (let i = 0; i < row.length; i++) {
-          const testPoint = {x: point.x + row[i], y: point.y + col[i]};
-          if (this.checkPixel(testPoint)) {
-            if (!this.checkedPixels.includes(this.getIndex(testPoint))) {
-              queue.push(testPoint);
-            }
-          } else {
-            this.fill.points.push({...point});
-          }
+        let x1 = point.x;
+        let color = this.context.getImageData(point.x, point.y, 1, 1).data;
+        while (x1 > 0 && this.checkColor(color)) {
+          x1--;
+          color = this.context.getImageData(x1, point.y, 1, 1).data;
         }
+        x1++;
+        this.fill.points.push({x: x1, y: point.y});
+        let spanAbove = false;
+        let spanBelow = false;
+        color = this.context.getImageData(x1, point.y, 1, 1).data;
+        while (x1 < this.drawing.clientWidth && this.checkColor(color)) {
+            let testColor = this.context.getImageData(x1, point.y - 1, 1, 1).data;
+            if (!spanAbove && point.y > 0 && this.checkColor(testColor)) {
+              if (!this.checkedPixels.includes(this.getIndex({x: x1, y: point.y - 1}))) {
+                queue.push({x: x1, y: point.y - 1});
+              }
+              spanAbove = true;
+            }
+            testColor = this.context.getImageData(x1, point.y + 1, 1, 1).data;
+            if (!spanBelow && point.y < this.drawing.clientHeight && this.checkColor(testColor)) {
+              if (!this.checkedPixels.includes(this.getIndex({x: x1, y: point.y + 1}))) {
+                queue.push({x: x1, y: point.y + 1});
+              }
+              spanBelow = true;
+            }
+            x1++;
+            color = this.context.getImageData(x1, point.y, 1, 1).data;
+        }
+        this.fill.points.push({x: x1, y: point.y});
       }
     }
 
     this.fill.draw();
     this.commands.execute(new AddSVGService(this.fill, this.svgStockage));
+  }
+
+  checkColor(color: Uint8ClampedArray): boolean {
+    return color[0] === this.color[0] && color[1] === this.color[1] && color[2] === this.color[2];
   }
 
   checkPixel(position: Point): boolean {
