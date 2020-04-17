a45e9e1f117c96f5bbb747ec7a31bab805364b1b
Scandium <samuel.couvrette@polymtl.ca>
2020-04-17 11:17:22 -0400

    100% coverage ellipse-tool

--- cadriciel/client/src/app/services/tools/basic-shape-tool/ellipse-tool.service.spec.ts
+++ cadriciel/client/src/app/services/tools/basic-shape-tool/ellipse-tool.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EllipseService } from '../../stockage-svg/draw-element/basic-shape/ellipse.service';
+import { ANGLE_VARIATION, EllipseService } from '../../stockage-svg/draw-element/basic-shape/ellipse.service';
+import { Point } from '../../stockage-svg/draw-element/draw-element';
+import { BasicShapeToolService } from './basic-shape-tool.service';
 import { EllipseToolService } from './ellipse-tool.service';
+import { ENDING_ANGLE, STARTING_ANGLE } from './polygon-tool.service';
 
 // tslint:disable: no-magic-numbers
 // tslint:disable: no-string-literal
 
 describe('EllipseToolService', () => {
   let service: EllipseToolService;
+  let basicShapeToolSpy: jasmine.Spy<() => void>;
+
   beforeEach(() => TestBed.configureTestingModule({}));
-  beforeEach(() => service = TestBed.get(EllipseToolService));
+  beforeEach(() => {
+    service = TestBed.get(EllipseToolService);
+    basicShapeToolSpy = spyOn(BasicShapeToolService.prototype, 'refreshSVG').and.callFake(() => { return; });
+  });
 
   // TESTS clear
 
@@ -49,5 +57,34 @@ describe('EllipseToolService', () => {
     service['calculatedHeight'] = 28;
     service.clear();
     expect(service['calculatedHeight']).toEqual(0);
+  });
+
+  // TESTS refreshSVG
+
+  it('#refreshSVG devrait actualiser le pointMin de l\'ellipse', () => {
+    service['shape'].points[0] = {x: 15, y: 22};
+    service.refreshSVG();
+    expect(service['shape'].pointMin).toEqual({x: 15, y: 22});
+  });
+  it('#refreshSVG devrait actualiser le pointMax de l\'ellipse', () => {
+    service['shape'].points[0] = {x: 18, y: 28};
+    service.refreshSVG();
+    expect(service['shape'].pointMin).toEqual({x: 18, y: 28});
+  });
+  it('#refreshSVG devrait ajouter des points sur tout le contour de l\'ellipse', () => {
+    service['shape'].points = [{x: 0, y: 0}, {x: 10, y: 10}];
+    const points: Point[] = [];
+    for (let angle = STARTING_ANGLE; angle < ENDING_ANGLE; angle += Math.PI / ANGLE_VARIATION) {
+      points.push({
+        x: 5 * Math.cos(angle) + 5,
+        y: 5 * Math.sin(angle) + 5
+      });
+    }
+    service.refreshSVG();
+    expect(service['shape'].points).toEqual(points);
+  });
+  it('#refreshSVG devrait appeler refreshSVG de BasicShapeToolService', () => {
+    service.refreshSVG();
+    expect(basicShapeToolSpy).toHaveBeenCalled();
   });
 });
