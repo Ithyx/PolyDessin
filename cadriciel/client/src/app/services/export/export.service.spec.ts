e6172b057174f57fb72a742d2b432b9bbca069a3
H. V <hugo.vandenbroucke-menu@polymtl.ca>
2020-04-18 18:46:53 -0400

    Ajout du beforeEach pour le context des tests de ExportService

--- cadriciel/client/src/app/services/export/export.service.spec.ts
+++ cadriciel/client/src/app/services/export/export.service.spec.ts
@@ -8,10 +8,19 @@ import { ExportService } from './export.service';
 
 describe('ExportService', () => {
   let service: ExportService;
+  let context: CanvasRenderingContext2D;
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientModule]
   }));
   beforeEach(() => service = TestBed.get(ExportService));
+  beforeEach(() => {
+    const canvas = document.createElement('canvas');
+    const contextCanvas = canvas.getContext('2d');
+    if (contextCanvas) {
+      context = contextCanvas;
+    }
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
@@ -20,41 +29,23 @@ describe('ExportService', () => {
   // TESTS drawAuthorCanvas
 
   it('#drawAuthorCanvas devrait modifier le style du context pour le nom d\'auteur', () => {
-    let context: CanvasRenderingContext2D;
-    const canvas = document.createElement('canvas');
-    const contextCanvas = canvas.getContext('2d');
-    if (contextCanvas) {
-      context = contextCanvas;
-      service.drawAuthorCanvas(context, '', 500);
-      expect(context.font).toEqual('30px Arial');
-      expect(context.strokeStyle).toEqual('#ffffff');
-      expect(context.lineWidth).toEqual(3);
-    }
+    service.drawAuthorCanvas(context, '', 500);
+    expect(context.font).toEqual('30px Arial');
+    expect(context.strokeStyle).toEqual('#ffffff');
+    expect(context.lineWidth).toEqual(3);
   });
 
   it('#drawAuthorCanvas devrait appeler strokeText du context', () => {
-    let context: CanvasRenderingContext2D;
-    const canvas = document.createElement('canvas');
-    const contextCanvas = canvas.getContext('2d');
-    if (contextCanvas) {
-      context = contextCanvas;
-      const spy = spyOn(context, 'strokeText');
-      const authorName = '';
-      service.drawAuthorCanvas(context, authorName, 500);
-      expect(spy).toHaveBeenCalledWith(`auteur: ${authorName}`, 0, 495);
-    }
+    const spy = spyOn(context, 'strokeText');
+    const authorName = '';
+    service.drawAuthorCanvas(context, authorName, 500);
+    expect(spy).toHaveBeenCalledWith(`auteur: ${authorName}`, 0, 495);
   });
 
   it('#drawAuthorCanvas devrait appeler strokeText du context', () => {
-    let context: CanvasRenderingContext2D;
-    const canvas = document.createElement('canvas');
-    const contextCanvas = canvas.getContext('2d');
-    if (contextCanvas) {
-      context = contextCanvas;
-      const spy = spyOn(context, 'fillText');
-      const authorName = '';
-      service.drawAuthorCanvas(context, authorName, 500);
-      expect(spy).toHaveBeenCalledWith(`auteur: ${authorName}`, 0, 495);
-    }
+    const spy = spyOn(context, 'fillText');
+    const authorName = '';
+    service.drawAuthorCanvas(context, authorName, 500);
+    expect(spy).toHaveBeenCalledWith(`auteur: ${authorName}`, 0, 495);
   });
 });
