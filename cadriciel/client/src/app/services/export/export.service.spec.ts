13c7340a171a927b6f46372ebaa22c8a76aceb6b
Mathis Lamidey <mathis-david.lamidey@polymtl.ca>
2020-04-18 17:41:05 -0400

    fix tests compilation export service

--- cadriciel/client/src/app/services/export/export.service.spec.ts
+++ cadriciel/client/src/app/services/export/export.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
 import { ExportService } from './export.service';
 
 describe('ExportService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: ExportService = TestBed.get(ExportService);
--- cadriciel/client/src/app/services/export/export.service.ts
+++ cadriciel/client/src/app/services/export/export.service.ts
@@ -129,6 +129,7 @@ export class ExportService {
       imageSrc = this.canvas.toDataURL('image/' + this.selectedExportFormat);
     }
     this.container.href = imageSrc;
+    console.log(this.selectedFileName);
     this.container.download = this.selectedFileName;
     this.container.click();
     URL.revokeObjectURL(imageSrc);
