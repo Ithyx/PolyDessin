078e3cb7c9f6accc31773bd7eb3c36bab1d27605
Scandium <samuel.couvrette@polymtl.ca>
2020-04-18 21:17:12 -0400

    tests de createCanvas et onImageLoad de paint-bucket-tool

--- cadriciel/client/src/app/services/tools/paint-bucket-tool.service.spec.ts
+++ cadriciel/client/src/app/services/tools/paint-bucket-tool.service.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CanvasConversionService } from '../canvas-conversion.service';
+import { AddSVGService } from '../command/add-svg.service';
 import { ColorFillService } from '../stockage-svg/draw-element/color-fill.service';
+import { SVGStockageService } from '../stockage-svg/svg-stockage.service';
 import { PaintBucketToolService } from './paint-bucket-tool.service';
 
 // tslint:disable:no-magic-numbers
@@ -12,6 +14,7 @@ describe('PaintBucketToolService', () => {
   let element: ColorFillService;
   let canvas: HTMLCanvasElement;
   let context: CanvasRenderingContext2D;
+  let data: Uint8ClampedArray;
 
   const svgElementStub: Partial<SVGElement> = {
     clientHeight: 500,
@@ -41,7 +44,7 @@ describe('PaintBucketToolService', () => {
     service.canvas = canvas;
     element = new ColorFillService();
     const imageData = new ImageData(2, 2);
-    const data = imageData.data;
+    data = imageData.data;
     data.set([
       1, 0, 0, 1,
       1, 0, 0, 1,
@@ -101,19 +104,123 @@ describe('PaintBucketToolService', () => {
     expect(spy).toHaveBeenCalledWith('2d');
   });
 
+  it('#createCanvas ne devrait rien faire si le dessin n\'est pas défini', () => {
+    delete service.drawing;
+    service['image'].src = 'http://localhost:9876/test';
+    service.createCanvas();
+    expect(service['image'].src).toEqual('http://localhost:9876/test');
+  });
+
+  it('#createCanvas ne devrait rien faire si le contexte n\'est pas défini', () => {
+    spyOn(canvas, 'getContext').and.callFake(() => null);
+    service['image'].src = 'http://localhost:9876/test';
+    service.createCanvas();
+    expect(service['image'].src).toEqual('http://localhost:9876/test');
+  });
+
   it('#createCanvas devrait assigner à context', () => {
     delete service['context'];
     service.createCanvas();
     expect(service['context']).toEqual(context);
   });
 
+  it('#createCanvas devrait charger la fonction onImageLoad dans l\'image', () => {
+    service.createCanvas();
+    expect(JSON.stringify(service['image'].onload)).toEqual(JSON.stringify(service.onImageLoad));
+  });
+
+  it('#createCanvas devrait charger l\'URL de l\'image du SVG dans le src de l\'image', () => {
+    service['image'].src = 'http://localhost:9876/test';
+    service.createCanvas();
+    expect(service['image'].src).not.toEqual('http://localhost:9876/test');
+  });
+
   // TESTS onImageLoad
 
   it('#onImageLoad devrait appeler la fonction drawImage', () => {
     const spy = spyOn(service['context'], 'drawImage').and.callThrough();
-    service['image'].onload = service.onImageLoad.bind(service);
     service.onImageLoad();
     expect(spy).toHaveBeenCalledWith(service['image'], 0, 0);
+  });
+
+  it('#onImageLoad devrait appeler la fonction getImageData de context pour obtenir les couleurs du dessin au complet', () => {
+    service.onImageLoad();
+    expect(service['context'].getImageData).toHaveBeenCalledWith(0, 0, svgElementStub.clientWidth, svgElementStub.clientHeight);
+  });
+
+  it('#onImageLoad devrait mettre le résultat de getImageData dans l\'attribut pixelData', () => {
+    service.onImageLoad();
+    expect(service['pixelData']).toEqual(data);
+  });
+
+  it('#onImageLoad devrait appeler getIndex avec le mousePosition', () => {
+    service['mousePosition'] = {x: 35, y: 25};
+    const spy = spyOn(service, 'getIndex');
+    service.onImageLoad();
+    expect(spy).toHaveBeenCalledWith({x: 35, y: 25});
+  });
+
+  it('#onImageLoad devrait assigner les valeurs correspondantes dans pixelData à l\'attribut color', () => {
+    spyOn(service, 'getIndex').and.returnValue(8);
+    service.onImageLoad();
+    expect(service['color']).toEqual([9, 0, 0]);
+  });
+
+  it('#onImageLoad ne devrait rien faire si color est identique à primaryColor de colorParameter', () => {
+    spyOn(service, 'getIndex').and.returnValue(8);
+    service['colorParameter'].primaryColor.RGBA = [9, 0, 0, 1];
+    const spy = spyOn(service, 'fillWithColor');
+    service.onImageLoad();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('#onImageLoad devrait appeler fillWithColor si color est différent de primaryColor en R', () => {
+    spyOn(service, 'getIndex').and.returnValue(8);
+    service['colorParameter'].primaryColor.RGBA = [1, 0, 0, 1];
+    const spy = spyOn(service, 'fillWithColor');
+    service.onImageLoad();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('#onImageLoad devrait appeler fillWithColor si color est différent de primaryColor en G', () => {
+    spyOn(service, 'getIndex').and.returnValue(8);
+    service['colorParameter'].primaryColor.RGBA = [9, 1, 0, 1];
+    const spy = spyOn(service, 'fillWithColor');
+    service.onImageLoad();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('#onImageLoad devrait appeler fillWithColor si color est différent de primaryColor en B', () => {
+    spyOn(service, 'getIndex').and.returnValue(8);
+    service['colorParameter'].primaryColor.RGBA = [9, 0, 1, 1];
+    const spy = spyOn(service, 'fillWithColor');
+    service.onImageLoad();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('#onImageLoad devrait appeler draw de fill si le nombre de points est supérieur à 0', () => {
+    spyOn(service, 'fillWithColor').and.returnValue();
+    service['fill'].points = [{x: 0, y: 0}];
+    const spy = spyOn(service['fill'], 'draw');
+    service.onImageLoad();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('#onImageLoad devrait appeler execute de commands avec le fill si le nombre de points est supérieur à 0', () => {
+    spyOn(service, 'fillWithColor').and.returnValue();
+    service['fill'].points = [{x: 0, y: 0}];
+    const spy = spyOn(service['commands'], 'execute');
+    service.onImageLoad();
+    const command = new AddSVGService([service['fill']], TestBed.get(SVGStockageService));
+    expect(spy).toHaveBeenCalledWith(command);
+  });
+
+  it('#onImageLoad ne devrait pas appeler draw de fill si le nombre de points est 0', () => {
+    spyOn(service, 'fillWithColor').and.returnValue();
+    service['fill'].points = [];
+    const spy = spyOn(service['fill'], 'draw');
+    service.onImageLoad();
+    expect(spy).not.toHaveBeenCalled();
   });
 
   // TESTS fillWithColor
